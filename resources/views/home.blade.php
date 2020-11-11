64ac54775bc2edc922eeafccfd1545bd00727091
aouman <k2.aouman@gmail.com>
2020-11-11 16:47:56 +0000

    le scroll du menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,46 +3,39 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   </head>
   <body>
     <div class="main-content">
       <header>
-        <nav class="navbar navbar-expand-sm bg-white">
-          <ul class="navbar-nav flex-row align-items-center">
-            <li class="nav-item ml-3">
-              <img class="logo-short" src="/images/logo.png" alt="Logo" style="width: 200px;">
-              <!--img class="logo img-fluid" src="./assets/images/logo-oschool-noir.png" alt=""-->
-            </li>
-            <li class="d-none d-md-block nav-item mr-3 ml-3">
-              <a href="#" class="nav-link">Nos écoles</a>
-            </li>
-            <li class="d-none d-md-block nav-item mr-3 ml-3">
-              <a href="#" class="nav-link">Business</a>
-            </li>
-          </ul>
-
-          <ul class="navbar-nav ml-auto">
-            <!--li class="d-none d-md-block nav-item">
-              <div class="p-2 bg-oschool text-white rounded text-center w-100">
-                <a href="#" class="text-decoration-none">
-                  Connectez-vous
-                </a>
-              </div>
-            </li-->
-            <li class="nav-item dropdown d-md-none">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarmenu" data-toggle="dropdown">
-                <i class="fas fa-bars hamburger"></i>
-              </a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Nos écoles</a>
-                <a class="dropdown-item" href="#">Business</a>
-                <a class="dropdown-item" href="#">Connectez-vous</a>
-              </div>
-            </li>
-
-          </ul>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" href="#">
+            <img class="logo-short" src="/images/logo.png" alt="Logo" style="width: 200px;">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse flex-column" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">Espace clients</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">Espace pharmatiens</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">Trouvé votre carré</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">Réjoignez nous</a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
 
@@ -61,7 +54,7 @@
             <div class="shadow p-4 text-center">
               <p class="h5">Un espace audio aménagé et équipé</p>
               <div class="p-3">
-                <img src="/images/logo.png" alt="" class="rounded-circl">
+                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
               </div>
             </div>
           </div>
@@ -78,10 +71,146 @@
         </div>
       </section>
 
+      <section class="container-fluid mt-4">
+        <h1 class="text-uppercase p-4">Un concept innovante</h1>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="shadow p-4 text-center">
+              <p class="h5">Un espace audio aménagé et équipé</p>
+              <div class="p-3">
+                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="container-fluid">
+        <h1 class="text-uppercase p-4">Un concept innovante</h1>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="shadow p-4 text-center">
+              <p class="h5">Un espace audio aménagé et équipé</p>
+              <div class="p-3">
+                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="container-fluid">
+        <h1 class="text-uppercase p-4">Un concept innovante</h1>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="shadow p-4 text-center">
+              <p class="h5">Un espace audio aménagé et équipé</p>
+              <div class="p-3">
+                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="container-fluid">
+        <h1 class="text-uppercase p-4">Un concept innovante</h1>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="shadow p-4 text-center">
+              <p class="h5">Un espace audio aménagé et équipé</p>
+              <div class="p-3">
+                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="container-fluid">
+        <h1 class="text-uppercase p-4">Un concept innovante</h1>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="shadow p-4 text-center">
+              <p class="h5">Un espace audio aménagé et équipé</p>
+              <div class="p-3">
+                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="container-fluid">
+        <footer>
+
+        </footer>
+      </section>
 
 
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script>
+      $(document).ready(function() {
+        $(window).bind('scroll', function() {
+          var navHeight = $(window).height();
+          if ($(window).scrollTop() > navHeight) {
+            $('nav').addClass('fixed');
+          } else {
+            $('nav').removeClass('fixed');
+          }
+        });
+      });
+    </script>
   </body>
 </html>
