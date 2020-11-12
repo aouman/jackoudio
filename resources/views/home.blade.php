3669072cf498413a9ab147925b90fb6490668253
aouman <k2.aouman@gmail.com>
2020-11-12 16:40:52 +0000

    mise en place de la section partenaire, avantage, concerpt inovant, le footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,19 +19,19 @@
           </button>
           <div class="collapse navbar-collapse flex-column" id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item active">
+              <li class="nav-item mr-3 ml-3 active">
                 <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item mr-3 ml-3">
                 <a href="#" class="nav-link">Espace clients</a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item mr-3 ml-3">
                 <a href="#" class="nav-link">Espace pharmatiens</a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item mr-3 ml-3">
                 <a href="#" class="nav-link">Trouvé votre carré</a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item mr-3 ml-3">
                 <a href="#" class="nav-link">Réjoignez nous</a>
               </li>
             </ul>
@@ -48,31 +48,161 @@
       </section>
 
       <section class="container-fluid">
-        <h1 class="text-uppercase p-4">Un concept innovante</h1>
-        <div class="row">
+        <h1 class="text-uppercase text-title text-center pt-md-5">Alliance de l’optique et de l'Audioprothèse</h1>
+        <p class="text-center h5 p-4">L'Audioprothèse est un marché en pleine croissance soutenu par une transformation du système avec l’arrivé du Rac Zéro, nécessité d’appareiller les personnes </p>
+        <!--div class="row">
           <div class="col-md-4">
-            <div class="shadow p-4 text-center">
-              <p class="h5">Un espace audio aménagé et équipé</p>
-              <div class="p-3">
-                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
+            <div class="shadow p-4 text-center mb-4">
+              <p class="h5">Jack Audio partenariat avec les meilleurs marques Siemens et Starkey, les leaders</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="shadow">
+              <div class="shadow p-4 text-center mb-4">
+                <p class="h5">Concept Gagnant/Gagnant, opticien connaît ces clients et capitalise sur son fond de commerce</p>
+
               </div>
             </div>
           </div>
           <div class="col-md-4">
             <div class="shadow">
+              <div class="shadow p-4 text-center mb-4">
+                <p class="h5">Audioprothésiste diplômé</p>
 
+              </div>
             </div>
           </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-        </div>
+        </div-->
       </section>
 
       <section class="container-fluid mt-4">
-        <h1 class="text-uppercase p-4">Un concept innovante</h1>
+        <h1 class="text-uppercase p-5 text-title text-center">jackaudio propose</h1>
+        <div class="row p-3">
+          <div class="col-md-2"></div>
+          <div class="col-md-4">
+            <p class="h6 shadow-sm p-4 rounded-lg mb-4 bg-white">Installation de la cabine au frais de Jack Audio <span></span></p>
+            <p class="h6 shadow-sm p-4 rounded-lg mb-4 bg-white">Gestion des dossier de prise en charge  <span></span></p>
+            <p class="h6 shadow-sm p-4 rounded-lg mb-4 bg-white">Stock <span></span></p>
+            <p class="h6 shadow-sm p-4 rounded-lg mb-4 bg-white">Equipement</p>
+          </div>
+          <div class="col-md-4">
+            <p class="h6 shadow-sm p-4 rounded-lg mb-4 bg-white">Accessoires <span></span></p>
+            <p class="h6 shadow-sm p-4 rounded-lg mb-4 bg-white">Présence d’un Audioprothésiste. <span></span></p>
+            <p class="h6 shadow-sm p-4 rounded-lg mb-4 bg-white">Rétrocession d’une partie du chiffre d’affaire généré <span></span></p>
+          </div>
+          <div class="col-md-2"></div>
+          <!--div class="col-md-4">
+            <div class="shadow p-4 text-center">
+              <p class="h5">Un espace audio aménagé et équipé</p>
+              <div class="p-3">
+                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
+              </div>
+            </div>
+          </div-->
+        </div>
+      </section>
+
+      <section class="container-fluid">
+        <div class="row bg-primary pt-4 pb-5">
+          <div class="col-md-12">
+            <h1 class="text-uppercase p-5 text-white text-center">Jack Audio partenariat avec les meilleurs marques</h1>
+          </div>
+        </div>
+        <div class="row position-relative bottom">
+          <div class="col-md-3"></div>
+          <div class="col-md-3">
+            <div class="shadow p-4 text-center bg-white mb-3">
+              <div class="p-3">
+                <img src="/images/siemens-logo-default.svg" alt="" class="rounded-circl" width="100">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="shadow p-4 text-center bg-white">
+              <div class="">
+                <img src="/images/logo-starkey.svg" alt="" class="rounded-circl" width="90">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3"></div>
+        </div>
+      </section>
+
+      <section class="container-fluid py-5">
+        <h1 class="text-uppercase p-5 text-title text-center">Un avantage concurrentiel certain</h1>
+        <div class="row">
+          <div class="col-md-2"></div>
+          <div class="col-md-4">
+
+                <img src="/images/Wireless-2-1024x1024.png" alt="" class="" width="300">
+          </div>
+          <div class="col-md-5">
+            <div class="shadow-sm p-4 text-center">
+              <p class="h5 text-justify">Proposez la double compétence: Celle d’opticien, et celle d’audioprothésiste. Les clients apprécieront la diversité et la praticité. Sachant que la vue et l’ouïe sont deux sens principaux, regrouper ces deux compétences au sein d’une même entité
+représente réellement un plus. Non seulement en terme de satisfaction clients, mais également en terme de concurrence.
+En tant qu’opticien vous proposez du sur-mesure. Pourquoi ne pas faire de même avec les prothèses auditives?</p>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <div class="shadow"></div>
+          </div>
+        </div>
+      </section>
+
+      <section class="container-fluid"ededed>
+        <div class="row bg-primary pt-4 pb-5">
+          <div class="col-md-12">
+            <h1 class="text-uppercase p-5 text-white text-center">Un concept innovante</h1>
+          </div>
+        </div>
+        <div class="row bottom position-relative">
+          <div class="col-md-12 d-flex flex-wrap">
+            <div class="p-3 w-21">
+              <div class="shadow text-center p-3 bg-white rounded-lg">
+                <p class="h5">Un espace audio aménagé et équipé</p>
+                <div class="p-3">
+                  <img src="/images/logo.png" alt="" class="rounded-circl" width="150">
+                </div>
+              </div>
+            </div>
+            <div class="p-3 w-21">
+              <div class="shadow text-center p-3 bg-white rounded-lg h-box">
+                <p class="h5 text-break">Un réseau qui vous accompagne chaque jour</p>
+                <div class="p-3">
+                  <img src="/images/logo.png" alt="" class="rounded-circl" width="150">
+                </div>
+              </div>
+            </div>
+            <div class="p-3 w-21">
+              <div class="shadow text-center p-3 bg-white rounded-lg h-box">
+                <p class="h5">Des produits uniques</p>
+                <div class="p-3">
+                  <img src="/images/logo.png" alt="" class="rounded-circl" width="150">
+                </div>
+              </div>
+            </div>
+            <div class="p-3 w-21">
+              <div class="shadow text-center p-3 bg-white rounded-lg h-box">
+                <p class="h5">Une visibilité</p>
+                <div class="p-3">
+                  <img src="/images/logo.png" alt="" class="rounded-circl" width="150">
+                </div>
+              </div>
+            </div>
+            <div class="p-3 w-21">
+              <div class="shadow text-center p-3 bg-white rounded-lg h-box">
+                <p class="h5">Un relais de croissance</p>
+                <div class="p-3">
+                  <img src="/images/logo.png" alt="" class="rounded-circl" width="150">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--section class="container-fluid">
+        <h1 class="text-uppercase p-5 text-title text-center">Un concept innovante</h1>
         <div class="row">
           <div class="col-md-4">
             <div class="shadow p-4 text-center">
@@ -93,106 +223,12 @@
             </div>
           </div>
         </div>
-      </section>
-
+      </section-->
       <section class="container-fluid">
-        <h1 class="text-uppercase p-4">Un concept innovante</h1>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="shadow p-4 text-center">
-              <p class="h5">Un espace audio aménagé et équipé</p>
-              <div class="p-3">
-                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
-              </div>
-            </div>
+        <footer class="row bg-dark">
+          <div class="col-md-12">
+            <p class="p-4 text-white">© Copyright 2020 Jackaudio – Politique de confidentialité – Une création de Locelot</p>
           </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="container-fluid">
-        <h1 class="text-uppercase p-4">Un concept innovante</h1>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="shadow p-4 text-center">
-              <p class="h5">Un espace audio aménagé et équipé</p>
-              <div class="p-3">
-                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="container-fluid">
-        <h1 class="text-uppercase p-4">Un concept innovante</h1>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="shadow p-4 text-center">
-              <p class="h5">Un espace audio aménagé et équipé</p>
-              <div class="p-3">
-                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="container-fluid">
-        <h1 class="text-uppercase p-4">Un concept innovante</h1>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="shadow p-4 text-center">
-              <p class="h5">Un espace audio aménagé et équipé</p>
-              <div class="p-3">
-                <img src="/images/logo.png" alt="" class="rounded-circl" width="100">
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="shadow">
-
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="container-fluid">
-        <footer>
-
         </footer>
       </section>
 
@@ -203,7 +239,7 @@
     <script>
       $(document).ready(function() {
         $(window).bind('scroll', function() {
-          var navHeight = $(window).height();
+          var navHeight = $(window).height() - 90;
           if ($(window).scrollTop() > navHeight) {
             $('nav').addClass('fixed');
           } else {
