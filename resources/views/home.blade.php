846453c61f552fde5564b3731d332eb6f1836f5d
aouman <k2.aouman@gmail.com>
2020-11-19 12:35:56 +0000

    logo du site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
       <header style="background: #f087b6;">
         <nav class="navbar navbar-expand-lg navbar-light">
           <a class="navbar-brand" href="/">
-            <img class="logo-short" src="/images/logo.png" alt="Logo" style="width: 120px;">
+            <img class="logo-short" src="/images/logo_1.png" alt="Logo" style="width: 120px;">
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
